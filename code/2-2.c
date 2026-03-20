96fee506558983ec2912ad21106a491c82e56a43
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-03-20 10:22:31 +0900

    2-2.ㅊ

---
+++ code/2-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void swap(int *x, int *y){
+    int num = *x;
+    *x = *y;
+    *y = num;
+}
+
+int main() {
+   int x = 100, y =200;
+
+   printf("x = %d, y = %d\n",x,y);
+
+   swap(&x, &y);
+   printf("x = %d, y = %d\n", x, y);
+
+   return 0;
+}
