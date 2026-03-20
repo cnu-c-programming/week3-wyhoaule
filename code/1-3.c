230a2455a6d0a398f3459cd247e094ae3cc15976
gdy <153972304+wyhoaule@users.noreply.github.com>
2026-03-20 09:56:54 +0900

    1-3.ㅊ

---
+++ code/1-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int power(int base, int exp) {
+    if (exp == 0) return 1;
+    return base * power(base, exp-1);
+}
+
+int main() {
+    printf("%d\n", power(2, 3));
+    printf("%d\n", power(5, 0));
+    printf("%d\n", power(3, 4));
+
+    return 0;
+}
