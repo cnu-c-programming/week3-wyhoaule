54b2259661a8fa8605105fe57ff393f71dfa9aa8
wyhoaule <153972304+wyhoaule@users.noreply.github.com>
2026-03-20 09:45:22 +0900

    1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    void Print();
+    void print();
     int square();
     int num = 5;
     int result = square(num);
+    print();
     printf("%d %d\n", num, result);
 
     return 0;
 }
 
-void Print() {
+void print() {
 printf("Welcome to Computer Programming 03 Lab!\n");
 }
 
